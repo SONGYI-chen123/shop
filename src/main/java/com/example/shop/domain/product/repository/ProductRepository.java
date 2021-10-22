f19510d52e4b0ba150b32dff3718ba4d5e26884e
Yichen <yichen.song@thoughtworks.com>
2021-10-22 15:39:16 +0800

    [yichen]: add method of findAll in repository

--- src/main/java/com/example/shop/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/shop/domain/product/repository/ProductRepository.java
@@ -2,6 +2,10 @@ package com.example.shop.domain.product.repository;
 
 import com.example.shop.domain.product.entity.Product;
 
+import java.util.List;
+
 public interface ProductRepository {
   void saveProduct(Product product);
+
+  List<Product> findAllProducts();
 }
--- src/main/java/com/example/shop/infrastructure/persistence/repository/JpaProductRepository.java
+++ src/main/java/com/example/shop/infrastructure/persistence/repository/JpaProductRepository.java
@@ -5,6 +5,9 @@ import com.example.shop.domain.product.entity.Product;
 import com.example.shop.domain.product.repository.ProductRepository;
 import com.example.shop.infrastructure.persistence.entity.ProductPo;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static com.example.shop.infrastructure.persistence.assembler.ProductDataMapper.mapper;
 
 public interface JpaProductRepository
@@ -12,5 +15,12 @@ public interface JpaProductRepository
   default void saveProduct(Product product) {
     this.save(mapper.toPo(product));
   }
+
+  @Override
+  default List<Product> findAllProducts() {
+    return findAll().stream().map(mapper::toDo).collect(Collectors.toList());
+  }
+
+  List<ProductPo> findAll();
 
 }
