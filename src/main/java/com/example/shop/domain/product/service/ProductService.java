a44ddb0684c86d284e5a35daa6b3c218adfffb1c
SONGYI-chen123 <song1577660501@163.com>
2022-12-05 18:17:43 +0800

    [yichen]: try.

--- src/main/java/com/example/shop/domain/product/service/ProductService.java
+++ src/main/java/com/example/shop/domain/product/service/ProductService.java
@@ -4,21 +4,17 @@ import com.example.shop.domain.product.entity.Product;
 import com.example.shop.domain.product.entity.ProductStockRecord;
 import com.example.shop.domain.product.repository.ProductRepository;
 import com.example.shop.domain.product.repository.ProductStockRecordRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@AllArgsConstructor
 public class ProductService {
   private final ProductRepository productRepository;
   private final ProductStockRecordRepository productStockRecordRepository;
-
-  public ProductService(ProductRepository productRepository,
-      ProductStockRecordRepository productStockRecordRepository) {
-    this.productRepository = productRepository;
-    this.productStockRecordRepository = productStockRecordRepository;
-  }
 
   public void addProduct(Product product) {
     Product originalProduct =
